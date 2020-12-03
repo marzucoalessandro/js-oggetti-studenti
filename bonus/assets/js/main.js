c296899b23c14c949c278837af4719532584ba03
Alessandro <marzucoalessandro1@gmail.com>
2020-12-03 19:08:44 +0100

    2° punto bonus

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -26,45 +26,44 @@ for (var key in studente) {
 // //adesso le stampiamo a schermo tramite il nostro h1;
 document.getElementById('stampa').innerHTML = contenitore;
 
+
+
+
+
 //punto 2;
 //creare un array di oggetti di studenti;
-var gruppoStudenti = [
+var gruppoStudenti = {
+  studente1: {
+  name: "Marco",
+  surname: "Gatti",
+  age: 22
+  },
+  studente2: {
+  name: "Luca",
+  surname: "Lolli",
+  age: 24
+  },
+  studente3: {
+  name: "Carlo",
+  surname: "Pari",
+  age: 26
+  }
 
-  {
-    name: "Marco",
-    surname: "Gatti",
-    age: 22
-  },
-  {
-    name: "Luca",
-    surname: "Lolli",
-    age: 24
-  },
-  {
-    name: "Carlo",
-    surname: "Pari",
-    age: 26
-  },
-    
-];
+};
+
+
+
+
 //cicliamo su tutti gli studenti e stampiamo per ognuno nome e cognome;
 // console.log(gruppoStudenti);
-//verificata l'esistenza del ns array andiamo a fare un ciclo for  e cerchiamo i valori che ci interessano;
+//verificata l'esistenza del ns array andiamo a fare un ciclo for in e cerchiamo i valori che ci interessano;
 
 contenitore2 = "";
-for (var i = 0; i < gruppoStudenti.length; i++) {
-  contenitore2 += gruppoStudenti[i].name + " " + gruppoStudenti[i].surname + "<br>";
-  // console.log(contenitore2);
+for (var key in gruppoStudenti) {
+  console.log(key);
+  contenitore2 += gruppoStudenti[key].name + " " + gruppoStudenti[key].surname + "<br>";
 }
-
-
 
 document.getElementById("stampa2").innerHTML = contenitore2;
 
 //a questo punto tramite tre variabili prompt prendiamo dei valori e inseriamoli in un nuovo oggetto nel nostro array.
-
-var newName = prompt("Qual'è il tuo nome?");
-var newSurname = prompt("qual'è il tuo cognome?");
-var newAge = Number(prompt("quanti anni hai?"));
-
-console.log(gruppoStudenti);
